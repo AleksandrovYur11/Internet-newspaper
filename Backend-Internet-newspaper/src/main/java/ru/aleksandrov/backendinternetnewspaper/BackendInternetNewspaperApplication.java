3dc20adc338c9551db00fb9dfb98355a3b2348be
AleksandrovYur11 <yurochka.aleksandrov.99@mail.ru>
2023-11-26 21:51:15 +0300

    init commit

---
+++ Backend-Internet-newspaper/src/main/java/ru/aleksandrov/backendinternetnewspaper/BackendInternetNewspaperApplication.java
@@ -0,0 +1,13 @@
+package ru.aleksandrov.backendinternetnewspaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendInternetNewspaperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendInternetNewspaperApplication.class, args);
+    }
+
+}
