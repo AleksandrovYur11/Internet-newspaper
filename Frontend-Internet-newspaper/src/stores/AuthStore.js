ded89c5bead13f8bf4426be309ffd56aa472c830
llziraell <98565914+llziraell@users.noreply.github.com>
2023-12-02 00:10:14 +0300

    add role separation

--- Frontend-Internet-newspaper/src/stores/AuthStore.js
+++ Frontend-Internet-newspaper/src/stores/AuthStore.js
@@ -2,12 +2,28 @@ import { defineStore } from "pinia"
 
 export const useAuthStore = defineStore("auth",  {
     state: () => ({
-        JWT: null
+        JWT: null, 
+        selectedRole: null,
+        modal: false,
+
     }),
     actions: {
         setAuthUser(responseData){
             this.JWT = responseData.accessToken
             // console.log(responseData)
+        },
+        selectRole(role){
+            this.selectedRole = role
+        },
+        sbrosRole(){
+            this.selectedRole = null
+            console.log(this.selectedRole)
+        },
+        showModal(){
+            this.modal = true
+        },
+        closeModal(){
+            this.modal = false
         }
     },
 })
