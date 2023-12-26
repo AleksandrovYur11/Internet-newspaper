10dc530aa388e63e82e268cd93b39be5829efbf1
AleksandrovYur11 <yurochka.aleksandrov.99@mail.ru>
2023-12-26 19:16:32 +0300

    fix deleteByToken on refreshTokenRepository

--- Backend-Internet-newspaper/src/main/java/ru/aleksandrov/backendinternetnewspaper/repositories/RefreshTokenRepository.java
+++ Backend-Internet-newspaper/src/main/java/ru/aleksandrov/backendinternetnewspaper/repositories/RefreshTokenRepository.java
@@ -2,13 +2,15 @@ package ru.aleksandrov.backendinternetnewspaper.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.aleksandrov.backendinternetnewspaper.models.RefreshToken;
 
 import java.util.Optional;
 @Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Integer> {
     Optional<RefreshToken> findByToken(String token);
-    void deleteByToken(RefreshToken token);
+    @Transactional
+    void deleteByToken(String token);
 }
 
 
