2b7067601e31500289006b77ef83b178ede3f223
AleksandrovYur11 <yurochka.aleksandrov.99@mail.ru>
2024-02-04 15:07:39 +0300

    add cacheable

--- Backend-Internet-newspaper/src/main/java/ru/aleksandrov/backendinternetnewspaper/BackendInternetNewspaperApplication.java
+++ Backend-Internet-newspaper/src/main/java/ru/aleksandrov/backendinternetnewspaper/BackendInternetNewspaperApplication.java
@@ -3,9 +3,11 @@ package ru.aleksandrov.backendinternetnewspaper;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableCaching
 public class BackendInternetNewspaperApplication {
 
     public static void main(String[] args) {
